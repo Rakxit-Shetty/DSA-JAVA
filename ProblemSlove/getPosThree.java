fa89d41c4179bb35fda329e3da57e368d3027942
Rakxit-Shetty <Rakshitshetty136@gmail.com>
2023-01-09 11:54:43 +0530

    impliment stack

---
+++ ProblemSlove/getPosThree.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class getPosThree{
+    public static void main(String args[]){
+        int a[]={-3,4,5,8,9,2,1,-6,6};
+        int n=a.length;//count=0;
+        ArrayList<Integer> b=new ArrayList<Integer>();
+       
+        for(int i=0;i<n;i++){
+            if(a[i]>0){
+                b.add(a[i]);
+            }
+        }
+            for(int i =0;i<b.size()-1;i++){
+                //System.out.println("\n"+b.get(i));
+                for(int j=0;j<n-i-1;j++){
+                    if(a[j]>a[j+1]){
+                            a[j]=a[j]+a[j+1];
+                            a[j+1]=a[j]-a[j+1];
+                            a[j]=a[j]-a[j+1];
+                    }
+                    
+                }
+            }
+
+       // Collections.sort(b);
+
+        int count=b.get(0);
+        
+
+
+    for(int i : b){
+    System.out.println(i);
+            /*if(i==count)
+            count++;
+            else{
+                break;
+            } */  
+        }
+
+        
+ //System.out.println("\n"+count);
+        }
+        
+
+    }
---
+++ Stack/stack.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+
+class mystack{
+         
+        public int top;
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> a= new ArrayList<Integer>();
+
+        mystack(){
+            top=-1;
+        }
+
+         void Show(){
+        if(a.isEmpty()){
+            System.out.println("STACK is empty");
+        }
+        else{
+            System.out.println("Elements are:");
+        for(int i=top;i>=0;i--)
+         System.out.println(a.get(i));
+    }
+    }
+        void pop(){
+        if(top==-1){
+            System.out.println("STACK is Empty ");
+        }
+        else{
+            System.out.println("stack pop sucess ele: "+a.get(top)+" poped");
+            top--;
+        }
+    }
+
+    void push(){
+    System.out.println("Enter number to push");
+    int ele=sc.nextInt();
+        top++;
+        a.add(top,ele);
+    }
+
+    }
+    
+public class stack{
+
+    public static void main(String args[]){
+           Scanner sc=new Scanner(System.in);
+            int run=1,choice;
+        
+            mystack my=new mystack();
+        while(run==1){
+            System.out.println("1.push\n2.pop\n3.Display\nEnter your choice:");
+            choice=sc.nextInt();
+
+            switch(choice){
+                case 1: 
+                my.push(); break;
+                case 2: 
+                my.pop(); 
+                break;
+                case 3:
+                my.Show(); break;
+
+            }
+            System.out.println("do you want to continue?: press 1 to cntn");
+            run=sc.nextInt();
+
+        }
+        
+    }
+    
+    
+    
+}
