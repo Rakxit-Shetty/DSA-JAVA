6cf553482c19888cecc1f5e7427bc06eeebe4f75
Rakxit-Shetty <Rakshitshetty136@gmail.com>
2023-01-11 21:18:25 +0530

    Queue update

---
+++ Queue/Queue.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+class myQueue{
+    int rear,front;
+    Scanner sc=new Scanner(System.in);
+    ArrayList<Integer> a=new  ArrayList<>();
+
+    myQueue(){
+        rear=-1;
+        front=-1;
+    }
+
+    void enqueue(){
+        
+        System.out.println("Enter the element");
+        if(front==-1)
+        front=0;
+        
+        
+        int ele=sc.nextInt();
+        rear++;
+        a.add(rear,ele);
+        System.out.println("element inserted:");
+    }
+
+    void dequeue(){
+        if(front==-1){
+            System.out.println("Empty Queue");
+            rear=-1;
+            front=-1;
+        }
+        else{
+            System.out.println("Element "+a.get(front)+" is deleted");
+            front++;
+            if(front>rear)
+            {
+                rear=-1;
+            front=-1;
+            }
+
+        }
+        
+    }
+
+    void display(){
+        if(rear==-1){
+                System.out.println("Empty queue");
+        }else{
+
+            for(int i=front;i<=rear;i++){
+            System.out.println("Element is :"+a.get(i));
+
+        }
+        }
+        
+    }
+}
+
+public class Queue{
+    public static void main(String args[]){
+
+ Scanner sc=new Scanner(System.in);
+            int run=1,choice;
+        
+            myQueue my=new myQueue();
+        while(run==1){
+            System.out.println("1.enqueue\n 2.dequeue\n 3.Display\n Enter your choice:");
+            choice=sc.nextInt();
+
+            switch(choice){
+                case 1: 
+                my.enqueue(); break;
+                case 2: 
+                my.dequeue(); 
+                break;
+                case 3:
+                my.display(); break;
+            }
+
+            System.out.println("do you want to continue?: press 1 to cntn");
+            run=sc.nextInt();
+
+        }
+
+    }
+}
---
+++ Sorting/selSort.java
@@ -0,0 +1,19 @@
+public class selSort
+{
+public static void main(String args[]){
+int a[]={1,3,2,5,4,7,6};
+int n=a.length;
+for(int i=0;i<n;i++){
+    int minpos=i;
+    for(int j=0;j<n;j++){
+        if(a[j]>a[minpos]){
+            int temp=a[j];
+            a[j]=a[minpos];
+            a[minpos]=temp;
+        }
+    }
+}
+for(int i : a)
+System.out.println(i);
+}
+}
