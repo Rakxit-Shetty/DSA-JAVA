b9202797f3ab634e58c6f85c34f1bb2c453bdd83
Rakxit-Shetty <Rakshitshetty136@gmail.com>
2023-01-14 21:21:27 +0530

    DeQueue update

--- DoublyQueue/DQueue.java
+++ DoublyQueue/DQueue.java
@@ -14,63 +14,111 @@ class myDqueue{
     }
 
     void ins_frnt(){
-        if(front==n){
+        if((front==0 && rear==n-1) || front==rear+1){
             System.out.println("QUEUE IS FULL");
         }
-        else{
+        else if((front==-1)&&(rear==-1)){
+            front=rear=0;
+            System.out.println("Enther the element to insert");
+        int ele=sc.nextInt();
+        a[front]=ele;
+        System.out.println("Element insert succesfuly");
+
+        }
+        else if(front==0){
+        front=n-1;
         System.out.println("Enther the element to insert");
         int ele=sc.nextInt();
-
-        front++;
         a[front]=ele;
         System.out.println("Element insert succesfuly");
         }
-    }
+        else{
+            System.out.println("Enther the element to insert");
+        int ele=sc.nextInt();
+        front--;
+        a[front]=ele;
+        System.out.println("Element insert succesfuly");
+        }
+        }
+    
 
     void del_front(){
-        if(front==-1){
+        if((front==-1) &&(rear==-1)){
         System.out.println("QUEUE IS EMPTY");
         }
-        else{
-            front--;
+        else if(front==rear){
+            front--; rear--;
             System.out.println("Element del sucessfuly");
         }
+        else if(front==(n-1)){
+            front=0;
+            System.out.println("Element del sucessfuly");
+        }
+        else{
+            System.out.println("Element del sucessfuly");
+            front++;
+        }
     }
+
 
 
     void ins_rear(){
-        if(front==n){
+        if((front==0 && rear==n-1) || (front==rear+1)){
             System.out.println("QUEUE IS FULL");
         }
-        else{
-        System.out.println("Enther the element to insert");
+        else if((front==-1) && (rear==-1)){
+        System.out.println("Enter the element to insert");
         int ele=sc.nextInt();
 
-        front++;
-        a[front]=ele;
+        rear=0;
+        a[rear]=ele;
         System.out.println("Element insert succesfuly");
+        }
+        else if(rear==n-1){
+        System.out.println("Enter the element to insert");
+        int ele=sc.nextInt();
+
+        rear=0;
+        a[rear]=ele;
+        System.out.println("Element insert succesfuly");
+
+        }
+        else{
+            System.out.println("Enter the element to insert");
+        int ele=sc.nextInt();
+            rear++;
+            a[rear]=ele;
         }
     }
     
     void del_rear(){
-        if(rear==-1){
+        if((rear==-1) && (front==-1)){
         System.out.println("QUEUE IS EMPTY");
         }
-        else{
-            rear++;
+        else if(front==rear){
+            rear=-1; front=-1;
             System.out.println("Element del sucessfuly");
+        }
+        else if(rear==0){
+            rear=n-1;
+            System.out.println("Element del sucessfuly");
+        }
+        else{
+            System.out.println("Element del sucessfuly");
+            rear--;
         }
     }
 
     void display(){
-        if(front==-1 || rear==-1){
-            System.out.println("QUEUE IS EMPTY");
-        }else{
-
-            for(int i=front; i<=rear;i++){
-            System.out.println("ele"+a[i]);
-        }
-        }
+         int i=front;    
+    System.out.println("\nElements in a deque are: ");    
+        
+    while(i!=rear)    
+    {    
+        System.out.println(a[i]);   
+        i=(i+1)%n;    
+    }    
+     System.out.println(a[rear]); 
         
     }
 
